a43efd153a473251c70cc35fc0b5b598bdd4bdaf
yagmurolmezoglu <yolmezoglu7@gmail.com>
2025-12-10 12:16:03 +0300

    Create dynamic table generator with random numbers
    This PHP script generates a table based on user-defined row and column counts, filling it with random numbers.

--- hafta12/index.php
+++ hafta12/index.php
@@ -1,1 +1,41 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>php tablo</title>
+</head>
+<body>
+
+<h3>Tablo Oluştur</h3>
+
+<form method="post">
+    Satır Sayısı: <input type="number" name="satir" min="1" required>
+    Sütun Sayısı: <input type="number" name="sutun" min="1" required>
+    <button type="submit">Tabloyu Oluştur</button>
+</form>
+
+<br>
+
+<?php
+if ($_POST) {
+    $satir = $_POST["satir"];
+    $sutun = $_POST["sutun"];
+
+    echo "<table border='1' cellpadding='10' cellspacing='0'>";
+
+    for ($i = 0; $i < $satir; $i++) {
+        echo "<tr>";
+        for ($j = 0; $j < $sutun; $j++) {
+            $rastgeleSayi = rand(1, 100);
+            echo "<td>$rastgeleSayi</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+?>
+
+</body>
+</html>
 
